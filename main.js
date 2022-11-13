a2723340158eabb7710151d8a21c3af5d85f0b20
Zahra1205 <78679374+Zahra1205@users.noreply.github.com>
2022-11-13 14:33:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function Bed_room(){
+    window.location.href = "https://zahra1205.github.io/bedroom/";
+}
+function tv(){
+    window.location.href = "https://zahra1205.github.io/TV_AC/";
+}
+function desk(){
+    window.location.href = "https://zahra1205.github.io/desk/";
+}
+function bottle(){
+    window.location.href = "https://zahra1205.github.io/bottle/";
+}
+function fruit(){
+    window.location.href = "https://zahra1205.github.io/fruit_basket/";
+}
